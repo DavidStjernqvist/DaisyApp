7f2ed83cbfb74569a68092ba747f61a4f2e5126e
David Stjernqvist <david.stjernqvist@se.experis.com>
2020-08-11 12:13:32 +0200

    First draft

---
+++ src/main/java/com/company/daisyapp/DaisyappApplication.java
@@ -0,0 +1,13 @@
+package com.company.daisyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisyappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaisyappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/daisyapp/DaisyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.daisyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaisyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
