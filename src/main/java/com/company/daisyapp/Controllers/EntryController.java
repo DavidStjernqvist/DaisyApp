3e93f3d81b68973f5829a49dcd0fbee8bc8ad1f0
David Stjernqvist <david.stjernqvist@se.experis.com>
2020-08-11 15:25:30 +0200

    Added index.html

--- src/main/java/com/company/daisyapp/Controllers/EntryController.java
+++ src/main/java/com/company/daisyapp/Controllers/EntryController.java
@@ -8,15 +8,13 @@ import com.company.daisyapp.Utils.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
+@RequestMapping("/api")
 public class EntryController {
 
     @Autowired
--- src/main/java/com/company/daisyapp/Controllers/HomeController.java
+++ src/main/java/com/company/daisyapp/Controllers/HomeController.java
@@ -1,7 +1,22 @@
 package com.company.daisyapp.Controllers;
 
+import com.company.daisyapp.Models.Entry;
+import com.company.daisyapp.Repositories.EntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.*;
 @Controller
 public class HomeController {
+
+    @Autowired
+    EntryRepository entryRepository;
+
+    @GetMapping("/")
+    public String index(Model model){
+        List<Entry> entriesList = entryRepository.findAll();
+        model.addAttribute("entriesList", entriesList);
+        return "index";
+    }
 }
